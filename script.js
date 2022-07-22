da416fa7621c73b535c55d5627b443ba0df1d9c2
codenoobies <kevindubidaam@gmail.com>
2022-07-22 13:44:56 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const message = [
+    "Halooo Raisa 🤮",
+    "Selamat ulang tahun, cieee🥺",
+    "Semoga jadi pribadi yg lebih baik, lebih sabar, lebih teliti, lebih teratur dan lebih bersih lagi hehe",
+    "Jangan kebanyakan ngegym nanti klo jadi cantik lalu punya pacar jd susah diajak nongki 😭",
+    "Hmmm btw mau kado sama kue apa ?",
+    "Oh gak usah, yauda gpp Ditunggu ya traktirannya 🥰"
+];
+const element = {
+    paragraph: document.querySelector(".Content"),
+    button: document.querySelector(".eventEmitter"),
+    container: document.querySelector(".button-container"),
+    randomPosition: null
+}
+
+let counter = 0;
+
+window.addEventListener("load", function () {
+    element.paragraph.innerText = message[counter];
+})
+
+function createButton() {
+    const button = document.createElement("button");
+    button.innerText = "Apa Aja Boleh";
+    button.classList.add("button", "randomPosition");
+    element.container.appendChild(button);
+    element.randomPosition = button;
+}
+
+function moveButton() {
+    const elementDimension = {
+        height: element.container.offsetHeight - 50,
+        width: element.container.offsetWidth - 200
+    }
+
+    const styling = element.randomPosition.style;
+    styling.left = Math.floor(Math.random() * elementDimension.width) + 1 + "px";
+    styling.top = Math.floor(Math.random() * elementDimension.height) + 1 + "px";
+    console.log(styling.left, styling.top)
+}
+
+element.container.addEventListener("click", function (event) {
+    if (event.target.classList.contains("eventEmitter")) {
+        if (counter < message.length - 1) {
+            counter++;
+            if (counter === 4) {
+                element.button.innerText = "Gak usah";
+                createButton();
+            };
+
+            if (counter === 5) {
+                element.randomPosition.remove();
+                element.button.innerText = "Okayy"
+                setTimeout(function () {
+                    location.href = "https://www.youtube.com/watch?v=n3qw7lZzbkM"
+                }, 2000)
+            }
+            element.paragraph.innerText = message[counter];
+        }
+        return;
+    }
+    if (event.target.classList.contains("randomPosition")) {
+        moveButton();
+    }
+})
+
+
